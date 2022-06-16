98d0e445cc809d1c3fb70593aa47e4ab74e97d2e
Lalit <lalitkumarsharma.jsr@gmail.com>
2022-06-16 18:40:03 +0530

    First demo

---
+++ src/components/First.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const First =()=>{
+  return(
+    <div>
+    <h1>Un-Employment</h1>
+    </div>
+  )
+}
+
+export default First
---
+++ src/components/content/content1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const content = () => {
+  return (
+    <div>
+      <h4>What is Unemployment?</h4>
+
+      <p>
+        Unemployment refers to a situation in which a skilled and talented people wanted to do a job. But cannot find a proper job due to several reasons.
+      </p>
+      
+    </div>
+  )
+}
+
+export default content
